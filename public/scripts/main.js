d6a2bf68dd5346a842bf74bffd48776f7ee39b78
Shaun Johnson <s@shaunjohnson.me>
2016-08-04 17:33:41 -0400

    Created dual paning

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,20 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
+ //Sticky Nav for the About Secitions
+$(document).ready(function() {
+  var stickyNavTop = $('#about-header').offset().top;
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();    
+    if (scrollTop > stickyNavTop) { 
+      $('#about-header').addClass('sticky');
+    } else {
+      $('#about-header').removeClass('sticky'); 
+    }
+  };
+  stickyNav(); 
+  $(window).scroll(function() {
+    stickyNav();
+  });
+});
+
 (function () {
   'use strict';
 
