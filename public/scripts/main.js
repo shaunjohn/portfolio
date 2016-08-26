6a27f5239dc2b5e7adb81c1f48705be5f77c0abb
Shaun Johnson <s@shaunjohnson.me>
2016-08-25 21:43:49 -0400

    Refactored js to ternary styling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,52 +1,42 @@
  //Sticky Nav for the About Sections
 $(document).ready(function() {
-  var stickyNavTop = $('#left_home').offset().top;
-  var stickyNavBottom = $('#blog').offset().top;
-  var stickyNav = function(){
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > stickyNavTop) { 
-      $('#left_home').addClass('sticky');
-    } else {
-      $('#left_home').removeClass('sticky'); 
-    } 
-
-    if (scrollTop > stickyNavBottom) { 
-      $('#left_home').removeClass('sticky');
-    } 
+  var stickyOneTop    = $('#sticky_one').offset().top, 
+      stickyOneBottom = $('#sticky_bottom1').offset().top,
+      stickyTwoTop    = $('#sticky_two').offset().top,
+      stickyTwoBottom = $('#sticky_bottom2').offset().top;
+  var stickyOnes = function(){
+    var windowTop = $(window).scrollTop();
+    ((windowTop > stickyOneTop) && (windowTop < stickyOneBottom)) ? $('#sticky_one').addClass('sticky') : $('#sticky_one').removeClass('sticky'); 
+    ((windowTop > stickyTwoTop) && (windowTop < stickyTwoBottom)) ? $('#sticky_two').addClass('sticky') : $('#sticky_two').removeClass('sticky'); 
   };
   $(window).scroll(function() {
-    stickyNav();
+    stickyOnes();
   });
 });
 
-(function () {
-  'use strict';
-
-  var querySelector = document.querySelector.bind(document);
-
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
-
+function menuMover() {
+  var querySelector      = document.querySelector.bind(document),
+      navdrawerContainer = querySelector('.navdrawer-container'),
+      body               = document.body,
+      appbarElement      = querySelector('.app-bar'),
+      menuBtn            = querySelector('.menu'),
+      main               = querySelector('main');
   function closeMenu() {
     body.classList.remove('open');
     appbarElement.classList.remove('open');
     navdrawerContainer.classList.remove('open');
   }
-
   function toggleMenu() {
     body.classList.toggle('open');
     appbarElement.classList.toggle('open');
     navdrawerContainer.classList.toggle('open');
   }
-
   main.addEventListener('click', closeMenu);
   menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
+  navdrawerContainer.addEventListener('click', function(event) {
     if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
       closeMenu();
     }
   });
-})();
+}
+menuMover();
